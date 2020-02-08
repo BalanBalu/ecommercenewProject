777b11241ba18c4e4a071fd2daf8de67f6ee01eb
Surya <arul@alldirectionsource.com>
2020-02-08 23:40:31 +0530

    make changes

--- api/app.js
+++ api/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-
+const bodyParser= require('body-parser')
+const userRoutes= require('./api/routes/user')
+app.use('/user',userRoutes)
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
 app.use((req,res,next)=>{
-    res.status(200).json({
-        message:'It works'
-    });
-});
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error,req,res,next)=>{
+res.status(error.status || 500);
+res.json({
+    error:{
+        message:error.message
+    }
+})
+    error.status = 404;
+    next(error);
+})
 
 module.exports = app
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,11 +2,34 @@ const  express = require("express")
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = require('../models/user')
-router.post('/signup',(req,res,next)=>{
+const bcrypt = require('bcrypt')
+router.post('/',(req,res,next)=>{
+    bcrypt.hash(req.body.password,10,(err,hash)=>{
+        if(err){
+           return  res.status(500).json({
+               error:err 
+           });
+        }else{
+   
 const user = new User({
     _id: new mongoose.Types.ObjectId(),
     email: req.body.email,
-    password:req.body.password
+    password:hash
+});
+user
+.save()
+.then(result=>{
+    res.status(201).json({
+        messge: 'User Created'
+    })
+})
+.catch(err=>{
+    console.log(err);
+    res.status(500).json({
+        error:err
+    })
+})
+        }
 })
 })
 
