47e1cd1f4679ae25dbbd731b0ea9c3c1a6900cc8
balan <balan4111995@gmail.com>
2020-02-10 09:36:25 -0800

    make changes

--- api/app.js
+++ api/app.js
@@ -1,25 +1,44 @@
 const express = require('express');
-const app = express();
 const bodyParser= require('body-parser')
-const userRoutes= require('./api/routes/user')
-app.use('/user',userRoutes)
+const app = express();
+const mongoose = require('mongoose')
+const userRoutes= require('./routes/user')
+mongoose.connect('mongodb+srv://balankottiyattil:'+process.env.MONGO_ATLAS_PW+'@ecommerceproject-pn3ry.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true })
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
-app.use((req,res,next)=>{
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
-})
-
-app.use((error,req,res,next)=>{
-res.status(error.status || 500);
-res.json({
-    error:{
-        message:error.message
+app.use('/user',userRoutes)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
     }
-})
+    next();
+  });
+  
+  // Routes which should handle requests
+  app.use("/user", userRoutes);
+  app.use((req, res, next) => {
+    const error = new Error("Not found");
     error.status = 404;
     next(error);
-})
-
-module.exports = app
+  });
+  
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+  });
+  
+  module.exports = app;
--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
-const signupSchema = mongoose.Schema({
-    _id:mongoose.Schema.Types.ObjectId,
-    email:{type:String,required:true},
-    password:{type:String,required:true} 
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: { type: String, required: true }
+});
 
-})
-
-module.exports = mongoose.model('user',signupSchema)
+module.exports = mongoose.model('User', userSchema);
